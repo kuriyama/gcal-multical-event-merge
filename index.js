3c2f57baf840024976b9c803d7712687df13db1d
hcawn <henrycawneilson@gmail.com>
2023-10-03 18:26:07 +0100

    wildcard field now ignores wildcard text and allowed events like 'busy' to match with ones at the same time

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ window.onload = function () {
 
     const getDisabled = () => new Promise(res => chrome.storage.local.get('disabled', (s) => res(s.disabled)));
     const getStyle = () => new Promise(res => chrome.storage.local.get('style', (s) => res(s.style)));
+    const getWildcard = () => new Promise(res => chrome.storage.local.get('wildcard', (s) => res(s.wildcard)));
 
     const setIcon = (disabled) => chrome.action.setIcon({
         path: disabled ? "icon-disabled.png" : "icon.png"
     });
 
-    const setDropdown = (id, value) => {    
+    const setDropdown = (id, value) => {
         let element = document.getElementById(id);
         element.value = value;
     }
 
-    const setToggle = (id, value) => {   
+    const setToggle = (id, value) => {
         let element = document.getElementById(id);
         element.checked = value
+    }
+
+    const setWildcard = (id, value) => {
+        let element = document.getElementById(id);
+        element.value = value
     }
 
     getDisabled().then(disabled => {
@@ -32,6 +38,14 @@ window.onload = function () {
         setDropdown('select_style',value)
     });
 
+    getWildcard().then(value => {
+        if (value == null) {
+            value = 'busy'
+            chrome.storage.local.set({ 'wildcard': value });
+        }
+        setWildcard('wildcard_value',value)
+    });
+
     const manifest_data = chrome.runtime.getManifest();
     document.getElementById('version').innerHTML=`V${manifest_data.version}`
 
@@ -46,4 +60,8 @@ window.onload = function () {
         chrome.storage.local.set({ 'style': e.target.value });
     };
 
+    document.getElementById('wildcard_value').onchange = function (e) {
+        if (e.target.value.length < 3 && e.target.value !== '') return alert('wildcard too short, you are going to get problems. If you do not want wildcard matching make this textbox blank')
+        chrome.storage.local.set({ 'wildcard': e.target.value });
+    };
 }
